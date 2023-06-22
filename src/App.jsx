ddd9a4db66ba86a5e7a8e80d5f0bd3b11647f492
wfercanas <fernando.canas@outlook.com>
2023-06-22 10:19:09 -0500

    feat(button): businessUnit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Button } from "./desing-system/Button";
 
 function App() {
-  console.log(import.meta.env.VITE_CLIENT);
+  const businessUnit = import.meta.env.VITE_CLIENT;
 
   return (
     <div>
       <h1>Personas</h1>
-      <p>Portal de clientes</p>
+      <p>Portal de clientes de {businessUnit}</p>
       <Button label="Iniciar sesión" />
     </div>
   );
