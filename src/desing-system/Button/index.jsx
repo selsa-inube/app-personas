ca2a15d94e3fdb75463771bf94a87c44876be1b2
wfercanas <fernando.canas@outlook.com>
2023-06-22 10:11:57 -0500

    feat(test): button

---
+++ src/desing-system/Button/index.jsx
@@ -0,0 +1,7 @@
+import { StyledButton } from "./styles";
+
+function Button({ label }) {
+  return <StyledButton>{label}</StyledButton>;
+}
+
+export { Button };
---
+++ src/desing-system/Button/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: yellow;
+`;
+
+export { StyledButton };
