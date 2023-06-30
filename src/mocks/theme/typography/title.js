56865fd8154b280106b676ed89b4950fe4a5611b
wfercanas <fernando.canas@outlook.com>
2023-06-30 17:30:01 -0500

    fix(typography): tracking with px units

--- src/mocks/theme/typography/title.js
+++ src/mocks/theme/typography/title.js
@@ -10,14 +10,14 @@ const title = {
     font: "Open Sans, sans-serif",
     lineHeight: "24px",
     size: "16px",
-    tracking: "0.15",
+    tracking: "0.15px",
     weight: "500",
   },
   small: {
     font: "Open Sans, sans-serif",
     lineHeight: "20px",
     size: "14px",
-    tracking: "0.1",
+    tracking: "0.1px",
     weight: "500",
   },
 };
