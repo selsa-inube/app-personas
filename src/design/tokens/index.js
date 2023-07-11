aebd3939fe55b5b633215309bdd45ed445e24033
wfercanas <fernando.canas@outlook.com>
2023-07-11 14:48:05 -0500

    feat(spacing): add spacing tokens

--- src/design/tokens/index.js
+++ src/design/tokens/index.js
@@ -1,9 +1,11 @@
 import { color } from "./color/color";
 import { typography } from "./typography/typography";
+import { spacing } from "./spacing/spacing";
 
 const inube = {
   color,
   typography,
+  spacing,
 };
 
 export { inube };
