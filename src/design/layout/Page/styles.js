aa5e786bfac4e9c3d96e209d01866c10e894a621
wfercanas <fernando.canas@outlook.com>
2023-07-14 17:37:56 -0500

    feat(page): allow scroll-y only in main

--- src/design/layout/Page/styles.js
+++ src/design/layout/Page/styles.js
@@ -10,10 +10,12 @@ const StyledPage = styled.div`
 const StyledContent = styled.div`
   display: grid;
   grid-template-columns: auto 1fr;
+  height: calc(100vh - 53px);
 `;
 
 const StyledMain = styled.main`
   padding: 32px 64px 64px;
+  overflow-y: scroll;
 `;
 
 export { StyledPage, StyledContent, StyledMain };
