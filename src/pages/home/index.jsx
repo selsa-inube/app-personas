3f5dbd6013684e2ddd83823c2dc18e62922776aa
wfercanas <fernando.canas@outlook.com>
2023-07-18 15:21:38 -0500

    feat(home): mockup for desktop

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,50 +1,22 @@
-import { QuickAccess } from "../../components/cards/QuickAccess";
-import { Text } from "../../design/data/Text";
-import { Stack } from "../../design/layout/Stack";
-
 import {
-  MdAdd,
-  MdAttachMoney,
-  MdCurrencyExchange,
-  MdHistory,
-  MdOutlineAccountBalance,
-  MdOutlineAddHome,
-  MdOutlineCreditScore,
-  MdOutlineSavings,
-  MdOutlineSupportAgent,
+  MdOutlineAccountBalanceWallet,
+  MdOutlineAttachMoney,
+  MdOutlineCreditCard,
 } from "react-icons/md";
 
-import { StyledGrid } from "./styles";
-import { Box } from "../../components/cards/Box";
+import { Text } from "../../design/data/Text";
+import { Stack } from "../../design/layout/Stack";
 import { inube } from "../../design/tokens";
+import { Box } from "../../components/cards/Box";
+import { Product } from "../../components/cards/Product";
+import { QuickAccess } from "../../components/cards/QuickAccess";
 
-const links = [
-  {
-    icon: <MdAttachMoney />,
-    label: "Paga tus créditos",
-    path: "/payments",
-  },
-  {
-    icon: <MdCurrencyExchange />,
-    label: "Transferir dinero",
-    path: "/transfer",
-  },
-  {
-    icon: <MdHistory />,
-    label: "Mis pagos automáticos",
-    path: "/debit",
-  },
-  {
-    icon: <MdOutlineAddHome />,
-    label: "Abrir CDT",
-    path: "/cdt",
-  },
-  {
-    icon: <MdOutlineSupportAgent />,
-    label: "Atención en línea",
-    path: "/support",
-  },
-];
+import { StyledGrid } from "./styles";
+
+import { quickLinks } from "../../config/quickLinks";
+import { savings, credits, cards } from "./config";
+
+import { savingsProducts, creditProducts, cardProducts } from "./mocks";
 
 function Home() {
   return (
@@ -53,51 +25,72 @@ function Home() {
         <Text type="title" as="h1">
           Bienvenido, Leonardo
         </Text>
-        <Text type="body" size="medium" color="gray">
+        <Text type="body" size="medium" appearance="gray">
           Aquí tienes un resumen de tus productos
         </Text>
       </Stack>
       <StyledGrid>
         <Stack direction="column" gap={inube.spacing.s300}>
           <Text type="label">Tus productos</Text>
-          <Box
-            title="Ahorros"
-            subtitle="Consulta tus cuentas"
-            icon={<MdOutlineSavings />}
-            collapsing={{
-              allow: false,
-            }}
-            button={{
-              label: "Solicitar ahorro",
-              icon: <MdAdd />,
-            }}
-          />
-          <Box
-            title="Créditos"
-            subtitle="Consulta tus préstamos"
-            icon={<MdOutlineAccountBalance />}
-            collapsing={{
-              allow: false,
-            }}
-            button={{
-              label: "Solicitar crédito",
-              icon: <MdAdd />,
-            }}
-          />
-          <Box
-            title="Tarjetas"
-            subtitle="Consulta tus compras"
-            icon={<MdOutlineCreditScore />}
-            collapsing={{
-              allow: false,
-            }}
-            button={{
-              label: "Solicitar tarjeta",
-              icon: <MdAdd />,
-            }}
-          />
+          <Box {...savings}>
+            <Stack direction="column" gap={inube.spacing.s075}>
+              {savingsProducts.length === 0 ? (
+                <Product
+                  empty={true}
+                  icon={<MdOutlineAccountBalanceWallet />}
+                />
+              ) : (
+                savingsProducts.map(({ title, id, attributes, tags }) => (
+                  <Product
+                    key={id}
+                    title={title}
+                    description={id}
+                    attributes={attributes}
+                    tags={tags}
+                    icon={<MdOutlineAccountBalanceWallet />}
+                  />
+                ))
+              )}
+            </Stack>
+          </Box>
+          <Box {...credits}>
+            <Stack direction="column" gap={inube.spacing.s075}>
+              {creditProducts.length === 0 ? (
+                <Product empty={true} icon={<MdOutlineAttachMoney />} />
+              ) : (
+                creditProducts.map(({ title, id, attributes, tags }) => (
+                  <Product
+                    key={id}
+                    title={title}
+                    description={id}
+                    attributes={attributes}
+                    tags={tags}
+                    icon={<MdOutlineAttachMoney />}
+                  />
+                ))
+              )}
+            </Stack>
+          </Box>
+          <Box {...cards}>
+            <Stack direction="column" gap={inube.spacing.s075}>
+              {cardProducts.length === 0 ? (
+                <Product icon={<MdOutlineCreditCard />} empty={true} />
+              ) : (
+                cardProducts.map(({ title, id, attributes, tags }) => (
+                  <Product
+                    key={id}
+                    title={title}
+                    description={id}
+                    attributes={attributes}
+                    tags={tags}
+                    icon={<MdOutlineCreditCard />}
+                  />
+                ))
+              )}
+            </Stack>
+          </Box>
         </Stack>
-        <QuickAccess links={links} />
+        <QuickAccess links={quickLinks} />
       </StyledGrid>
     </>
   );
