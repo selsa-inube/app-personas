f07edabc13d69bfa40585d517e0c30c0ace68524
wfercanas <fernando.canas@outlook.com>
2023-07-19 23:52:44 -0500

    feat(home): configuration of boxes

---
+++ src/pages/home/config/boxes.jsx
@@ -0,0 +1,44 @@
+import {
+  MdAdd,
+  MdOutlineAccountBalance,
+  MdOutlineCreditScore,
+  MdOutlineSavings,
+} from "react-icons/md";
+
+const savings = {
+  title: "Ahorros",
+  subtitle: "Consulta tus cuentas",
+  icon: <MdOutlineSavings />,
+  collapsing: { allow: false },
+  button: {
+    label: "Solicitar ahorro",
+    icon: <MdAdd />,
+    path: "/savings",
+  },
+};
+
+const credits = {
+  title: "Créditos",
+  subtitle: "Consulta tus préstamos",
+  icon: <MdOutlineAccountBalance />,
+  collapsing: { allow: false },
+  button: {
+    label: "Solicitar crédito",
+    icon: <MdAdd />,
+    path: "/credit",
+  },
+};
+
+const cards = {
+  title: "Tarjetas",
+  subtitle: "Consulta tus compras",
+  icon: <MdOutlineCreditScore />,
+  collapsing: { allow: false },
+  button: {
+    label: "Solicitar tarjeta",
+    icon: <MdAdd />,
+    path: "/cards",
+  },
+};
+
+export { savings, credits, cards };
