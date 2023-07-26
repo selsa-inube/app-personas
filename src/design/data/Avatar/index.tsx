5d17598539b77a0ed1f03b7c2a596fa8a0cd40e3
Cristian Rojas <crisrouges47@gmail.com>
2023-07-26 08:48:52 -0500

    feat(avatar): migrate avatar to typescript

--- src/design/data/Avatar/index.tsx
+++ src/design/data/Avatar/index.tsx
@@ -1,14 +1,14 @@
 import { MdPersonOutline } from "react-icons/md";
 import { Icon } from "../Icon";
 
-function Avatar() {
+function Avatar(): JSX.Element {
   return (
     <Icon
       icon={<MdPersonOutline />}
       variant="filled"
       shape="circle"
       spacing="compact"
-      allowHover={true}
+      parentHover={true}
     />
   );
 }
