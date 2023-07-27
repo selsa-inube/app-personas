99607f7cfe8a7722551815f0204c1d382583635b
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2023-07-27 09:23:14 -0500

    feat(config): added components alias path

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 import vitesconfigPaths from "vite-tsconfig-paths";
 
@@ -7,5 +8,10 @@ export default defineConfig({
   plugins: [react(), vitesconfigPaths()],
   server: {
     open: true,
+  },
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
   },
 });
