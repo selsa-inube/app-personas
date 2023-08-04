f7f99944ca56a40513a6702a7381798b7078e160
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2023-08-04 13:46:49 -0500

    feat(themes): added cooservunal theme

---
+++ src/mocks/design/themes/index.ts
@@ -0,0 +1,9 @@
+import { cooservunal } from "@mocks/design/themes/cooservunal";
+import { fondecom } from "@mocks/design/themes/fondecom";
+
+const themes = {
+  fondecom,
+  cooservunal,
+};
+
+export { themes };
