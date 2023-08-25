42b2625263afeb359a7d6844c0f4b87b99d4eb47
Cristian Rojas <crisrouges47@gmail.com>
2023-08-25 15:20:15 -0500

    feat(myInvestments): create myInvestments page

---
+++ src/pages/admin/investments/MyInvestments/config/products.tsx
@@ -0,0 +1,23 @@
+import { IProduct } from "@ptypes/pages/product.types";
+
+const myInvestmentAttributes = [
+  "investment-CDAT",
+  "investment-CDT"
+];
+
+function extractMyInvestmentAttributes(investment: IProduct) {
+  return investment.attributes.filter((attribute) =>
+    myInvestmentAttributes.includes(attribute.id)
+  );
+}
+
+const myInvestmentAttributeBreakpoints = {
+  "(min-width: 280px)": 3,
+  "(min-width: 950px)": 2,
+  "(min-width: 895px)": 1,
+  "(min-width: 750px)": 3,
+  "(min-width: 650px)": 2,
+  "(max-width: 660px)": 1,
+};
+
+export { extractMyInvestmentAttributes, myInvestmentAttributeBreakpoints };
