ccaeea6ed4612dc2b604c63fd384d92f8f4b32a3
Cristian Rojas <crisrouges47@gmail.com>
2023-08-30 08:16:41 -0500

    feat(breadcrumbs): improve breadcrumbs styles

--- src/design/navigation/Breadcrumbs/BreadcrumbMenuLink/styles.ts
+++ src/design/navigation/Breadcrumbs/BreadcrumbMenuLink/styles.ts
@@ -5,16 +5,19 @@ import { inube } from "@design/tokens";
 const StyledContainerLink = styled.li`
   display: inline-block;
   > * {
-    height: ${inube.spacing.s400};
+    padding: ${inube.spacing.s100} ${inube.spacing.s150};
     > label {
       cursor: pointer;
-      padding: 8px 12px 8px 12px;
     }
   }
 
   & p {
     color: ${({ theme }) =>
       theme.color?.text?.gray?.regular || inube.color.text.gray.regular};
+      max-width: 136px;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
   }
 `;
 
