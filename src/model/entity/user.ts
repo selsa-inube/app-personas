bd7c26571d095db4fff13545cb4dafaf3d93dd3e
CristianRojas-47 <crisrouges47@gmail.com>
2023-09-15 09:11:39 -0500

    feat(usersMock): mock personal data for user in an object

---
+++ src/model/entity/user.ts
@@ -0,0 +1,57 @@
+interface IIdentification {
+  firstName: string;
+  secondName?: string;
+  firstLastName: string;
+  secondLastName?: string;
+  type: string;
+  number: string;
+  place?: string;
+  date?: string;
+}
+
+interface IPersonalData {
+  identification: IIdentification[];
+  birthDate: string;
+  birthCity: string;
+  gender: string;
+  maritalStatus: string;
+  bloodType: string;
+}
+
+interface IContactData {
+  address: string;
+  department: string;
+  city: string;
+  zipCode?: string;
+  landlinePhone?: string;
+  cellPhone: string;
+  email: string;
+}
+
+interface IFamilyThird {
+  identification: IIdentification[];
+  contact: IContactData[];
+  information: {
+    birthDate: string;
+    gender: string;
+    relationship: string;
+    isDependent?: boolean;
+    educationLevel?: string;
+    businessActivity?: string;
+    profession?: string;
+  };
+}
+
+interface IThird {
+  personalData: IPersonalData[];
+  contact: IContactData[];
+  familyGroup?: IFamilyThird[];
+}
+
+export type {
+  IIdentification,
+  IPersonalData,
+  IContactData,
+  IFamilyThird,
+  IThird,
+};
--- src/pages/admin/investments/Investment/config/box.tsx
+++ src/pages/admin/investments/Investment/config/box.tsx
@@ -1,4 +1,4 @@
-import { ProductType } from "@ptypes/pages/product.types";
+import { ProductType } from "src/model/entity/product";
 import { MdOutlinePaid } from "react-icons/md";
 
 const investmentBoxButton = (type: ProductType) =>
