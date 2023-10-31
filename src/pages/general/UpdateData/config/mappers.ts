b9b5a217c8989a095f8b8499d6c82bd8e48adc8a
CristianRojas-47 <crisrouges47@gmail.com>
2023-10-31 14:51:45 -0500

    feat(personalReferencesForm): adjust the editing button functionality

--- src/pages/general/UpdateData/config/mappers.ts
+++ src/pages/general/UpdateData/config/mappers.ts
@@ -121,24 +121,23 @@ const mapPersonalDebts = (
   return personalDebts.map(
     (personalDebt, index) => mapPersonalDebt(personalDebt, index) as IEntry
   );
-}
+};
 
 const mapPersonalReference = (
-  personalAsset: IPersonalReferenceEntry,
+  personalReference: IPersonalReferenceEntry,
   index: number
 ): IEntry | IPersonalReferenceEntry => {
   return {
-    id: personalAsset.id || String(index),
+    id: personalReference.id || String(index),
     referenceType: getValueOfDomain(
-      personalAsset.referenceType || "",
+      personalReference.referenceType || "",
       "referenceType"
     )?.value,
-    name: personalAsset.name,
-    address: personalAsset.address,
-    email: personalAsset.email,
-    phone: personalAsset.phone,
-    city: cityDM.valueOf(personalAsset.city || "")?.value,
-    observations: personalAsset.observations,
+    name: personalReference.name,
+    address: personalReference.address,
+    email: personalReference.email,
+    phone: personalReference.phone,
+    city: cityDM.valueOf(personalReference.city || "")?.value,
   };
 };
 
