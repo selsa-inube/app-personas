6ae78adc57aa5db3eaf378c8e6c2509ea119d816
Johan Daniel Garcia Nova <gtahghfsg@gmail.com>
2023-12-19 09:52:52 -0500

    feat: generate manifest for PWA with Assets

---
+++ manifest.js
@@ -0,0 +1,36 @@
+const manifestForPlugIn = {
+  registerType: "prompt",
+  manifest: {
+    name: "Personas",
+    short_name: "Personas",
+    description: "Personas Description",
+    lang: "en-ES",
+    theme_color: "#fff",
+    background_color: "#fff",
+    display: "standalone",
+    display_override: ["window-controls-overlay"],
+    scope: "/",
+    start_url: "/",
+    icons: [
+      {
+        src: "fondecom-64x64.png",
+        sizes: "64x64",
+        type: "image/png",
+      },
+      {
+        src: "fondecom-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "fondecom-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+  },
+};
+
+export { manifestForPlugIn };
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import path from "path";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import vitesconfigPaths from "vite-tsconfig-paths";
+import { manifestForPlugIn } from "./manifest.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(), vitesconfigPaths()],
+  plugins: [react(), VitePWA(manifestForPlugIn), vitesconfigPaths()],
   server: {
     open: true,
     port: 3000,
