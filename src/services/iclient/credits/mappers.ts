ea836742ff4999ac38e3e1f954e513801a50d649
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2024-01-05 14:15:54 -0500

    feat(credits): get real amortization for credits

--- src/services/iclient/credits/mappers.ts
+++ src/services/iclient/credits/mappers.ts
@@ -1,5 +1,5 @@
 import { TagProps } from "@design/data/Tag";
-import { IMovement, IProduct } from "src/model/entity/product";
+import { IAmortization, IMovement, IProduct } from "src/model/entity/product";
 import { formatPrimaryDate } from "src/utils/dates";
 import { capitalizeText } from "src/utils/texts";
 
@@ -52,17 +52,30 @@ const mapCreditApiToEntity = (credit: Record<string, any>): IProduct => {
   const today = new Date();
   today.setHours(0, 0, 0, 0);
 
+  const heightQuota = credit.heightQuota.split(" ");
+  const maxQuota = heightQuota.length > 2 ? heightQuota[2] : 0;
+
+  const nextPaymentValue = credit.valueExpired?.totalPending
+    ? credit.valueExpired?.totalPending
+    : credit.nextPaymentValue.totalPending;
+
   const attributes = [
     {
       id: "net_value",
       label: "Saldo total",
-      value: Number(credit.balanceObligation.TotalPending),
+      value: Number(credit.balanceObligation.totalPending),
     },
     {
       id: "next_payment_date",
       label: "Fecha próximo pago",
       value: formatPrimaryDate(nextPaymentDate),
     },
+    {
+      id: "next_payment_value",
+      label: "Próximo pago",
+      value: nextPaymentValue,
+    },
+    { id: "terms", label: "Plazo", value: `${maxQuota} Meses` },
   ];
 
   const tags: TagProps[] =
@@ -97,7 +110,33 @@ const mapCreditsApiToEntities = (
   return credits.map((credit) => mapCreditApiToEntity(credit));
 };
 
+const mapCreditAmortizationApiToEntity = (
+  payment: Record<string, any>
+): IAmortization => {
+  return {
+    id: payment.paymentPlanId,
+    paymentNumber: payment.quotaNumber,
+    date: formatPrimaryDate(new Date(payment.quotaDate)),
+    capitalPayment: payment.capitalValue,
+    interest: payment.fixedInterestValue,
+    lifeInsurance: 0,
+    patrimonialInsurance: 0,
+    capitalization: 0,
+    others: payment.variableInterestValue,
+    totalMonthlyValue: payment.quotaValue,
+    projectedBalance: payment.projectedBalance,
+  };
+};
+
+const mapCreditAmortizationApiToEntities = (
+  payments: Record<string, any>[]
+): IAmortization[] => {
+  return payments.map((payment) => mapCreditAmortizationApiToEntity(payment));
+};
+
 export {
+  mapCreditAmortizationApiToEntities,
+  mapCreditAmortizationApiToEntity,
   mapCreditApiToEntity,
   mapCreditMovementApiToEntity,
   mapCreditMovementsApiToEntities,
