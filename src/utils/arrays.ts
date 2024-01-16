681d1af2d33f18be6816aaf4343b39b958e65107
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2024-01-15 19:53:12 -0500

    fix: remove test

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -3,7 +3,6 @@ const compareArraysByProperty = (
   arr2: Record<string, string | number | object>[],
   property: string,
 ) => {
-  const test = "si"
   if (arr1.length !== arr2.length) return false;
 
   return arr1.every((item, index) => item[property] === arr2[index][property]);
