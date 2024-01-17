7e55d312336f40514bafbd84ffcb00a23fb3f38e
Angie Pinilla <88405711+ANGIECATHERINEP@users.noreply.github.com>
2024-01-17 18:57:35 -0500

    fix(mappers): adjust conflicts in branch

--- src/services/iclient/credits/mappers.ts
+++ src/services/iclient/credits/mappers.ts
@@ -107,6 +107,11 @@ const mapCreditApiToEntity = (
   const normalizedPaymentMethodName = capitalizeText(
     String(credit.paymentMethodName).toLowerCase(),
   );
+  const interesRate =
+    Object(credit.accumulatedByObligations)[0].spreadCurrentRate +
+    Object(credit.accumulatedByObligations)[0].currentFixedPoints;
+
+  const roundInteresRate = interesRate.toFixed(2);
 
   const attributes = [
     {
@@ -160,6 +165,12 @@ const mapCreditApiToEntity = (
       value: amortizationTypeValuesMock[String(credit.amortization)],
     },
     { id: "terms", label: "Plazo", value: `${maxQuota} Meses` },
+
+    {
+      id: "interest_rate",
+      label: "Tasa de interés",
+      value: `${roundInteresRate} % NAMV`,
+    },
   ];
 
   if (inArrears) {
