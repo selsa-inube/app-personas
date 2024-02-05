e8bd8bf7eda667b918734b94899ceacc6f33ed79
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2024-02-05 14:16:51 -0500

    refactor(credits): split quota in different attrs

--- src/services/iclient/credits/getCredits/mappers.ts
+++ src/services/iclient/credits/getCredits/mappers.ts
@@ -17,8 +17,8 @@ const mapCreditApiToEntity = (
   const today = new Date();
   today.setUTCHours(5, 5, 5, 5);
 
-  const currentQuota = credit.duesPaid;
-  const maxQuota = credit.outstandingDues;
+  const duesPaid = credit.duesPaid;
+  const outstandingDues = credit.outstandingDues;
 
   const inArrears = today > nextPaymentDate;
 
@@ -77,9 +77,14 @@ const mapCreditApiToEntity = (
       value: nextPaymentValue,
     },
     {
-      id: "quote",
-      label: "Altura de cuota",
-      value: `${currentQuota} de ${maxQuota}`,
+      id: "dues_paid",
+      label: "Cuotas pagadas",
+      value: duesPaid,
+    },
+    {
+      id: "outstanding_dues",
+      label: "Cuotas pendientes",
+      value: outstandingDues,
     },
     {
       id: "peridiocity",
@@ -106,7 +111,11 @@ const mapCreditApiToEntity = (
       label: "Tipo de garantía",
       value: guaranteeTypeValuesMock[Object(credit.typeOfGuarantee).code],
     },
-    { id: "terms", label: "Plazo", value: `${maxQuota} Meses` },
+    {
+      id: "terms",
+      label: "Plazo",
+      value: `${Number(duesPaid) + Number(outstandingDues)} Meses`,
+    },
 
     {
       id: "interest_rate",
