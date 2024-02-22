97626ea792ed5eede34bb323153b5c057e2b11fd
CristianRojas-47 <crisrouges47@gmail.com>
2024-02-22 15:22:03 -0500

    refactor(savings): add gmfTypeValue options

--- src/mocks/products/savings/utils.mocks.ts
+++ src/mocks/products/savings/utils.mocks.ts
@@ -4,5 +4,9 @@ const estateTypeValuesMock: Record<string, string> = {
 
 const gmfTypeValuesMock: Record<string, string> = {
   AllTransactionsAreTaxed: "Todas las transacciones",
+  TotallyExcempt: "Totalmente exento",
+  ExcemptUntilMaximum: "Exento hasta el máximo",
+  ExcemptUntilMaximumOnPensionAllowance: "Exento hasta el máximo de pensión",
 };
+
 export { estateTypeValuesMock, gmfTypeValuesMock };
