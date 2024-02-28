b2dffb520233a565fb6b3130a15fc8eece434e0f
Johan Daniel Garcia Nova <gtahghfsg@gmail.com>
2024-02-28 15:57:53 -0500

    feat(savings): add attribute request date in programmedSavings

--- src/services/iclient/savings/getSavings/utils.ts
+++ src/services/iclient/savings/getSavings/utils.ts
@@ -182,6 +182,11 @@ const getProductAttributes = (
         label: "Beneficiarios",
         value: beneficiaries,
       },
+      {
+        id: "request_date",
+        label: "Fecha de apertura",
+        value: formatPrimaryDate(new Date(String(saving.creationDate))),
+      },
     ],
   };
 
