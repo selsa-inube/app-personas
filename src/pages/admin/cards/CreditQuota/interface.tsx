d2976d70e62faa5c6f39c1c15a18574012adbc00
CristianRojas-47 <crisrouges47@gmail.com>
2024-03-22 08:14:18 -0500

    feat(consumptionTable): adjust type attribute validation name

--- src/pages/admin/cards/CreditQuota/interface.tsx
+++ src/pages/admin/cards/CreditQuota/interface.tsx
@@ -67,7 +67,7 @@ function CreditQuotaUI(props: CreditQuotaUIProps) {
     (attr) => attr.id === "full_payment",
   )?.value;
 
-  const isRevolving = selectedProduct.creditQuota.attributes.find(
+  const cardType = selectedProduct.creditQuota.attributes.find(
     (attr) => attr.id === "type",
   )?.value;
 
@@ -169,7 +169,7 @@ function CreditQuotaUI(props: CreditQuotaUIProps) {
             </Stack>
           )}
 
-          {selectedConsumption && isRevolving !== "Rotativo" && (
+          {selectedConsumption && cardType !== "Rotativo" && (
             <Stack direction="column" gap="s300">
               <Text type="title" size="medium">
                 Consumos vigentes
