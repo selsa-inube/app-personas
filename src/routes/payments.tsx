e87582989901b1a530617728c90195dbe16fc957
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2024-04-23 16:07:17 -0500

    feat(payments): redirect pay on invalid url

--- src/routes/payments.tsx
+++ src/routes/payments.tsx
@@ -1,12 +1,15 @@
 import { Pay } from "@pages/admin/payments/Pay";
 import { PaymentOptions } from "@pages/admin/payments/PaymentOptions";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function PaymentsRoutes() {
   return (
     <Routes>
       <Route path="/" element={<PaymentOptions />} />
       <Route path="/pay" element={<Pay />} />
+      <Route path="/history" element={<></>} />
+
+      <Route path="*" element={<Navigate to="/payments/pay" replace />} />
     </Routes>
   );
 }
