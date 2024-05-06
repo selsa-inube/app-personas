5095a7dcf50f5f93dec99d5fa2ee763817e114ee
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2024-05-06 15:55:31 -0500

    feat(payments): added obligation payments

--- src/config/enviroment.ts
+++ src/config/enviroment.ts
@@ -10,13 +10,14 @@ const enviroment = {
   REDIRECT_URI: IS_PRODUCTION ? window.location.origin : AUTH_REDIRECT_URI,
   ICLIENT_API_URL_QUERY: import.meta.env.VITE_ICLIENT_API_URL_QUERY,
   ICLIENT_API_URL_PERSISTENCE: import.meta.env.VITE_ICLIENT_API_URL_PERSISTENCE,
-  BUSINESS_UNIT: import.meta.env.PROD ? "fondecom" : "test",
+  BUSINESS_UNIT: import.meta.env.PROD ? "fondecom" : "fondecom",
   APP_CODE: "personas",
   FEATURE_FLAGS_URL: import.meta.env.VITE_FEATURE_FLAGS_URL,
   FEATURE_FLAGS_KEY: import.meta.env.VITE_FEATURE_FLAGS_KEY,
   ANALYTICS_URL: import.meta.env.VITE_ANALYTICS_URL,
   ANALYTICS_KEY: import.meta.env.VITE_ANALYTICS_KEY,
-  TEAMS_WEBHOOK: "https://senlinea.webhook.office.com/webhookb2/34a9dbd2-ebc4-48c4-8821-9497fa2c3b47@274e76db-8d04-4110-abca-44dc583def79/IncomingWebhook/43f7251bcf3c4dfe923500adc52837cc/523e4558-3102-4d0d-b633-e00a21cc58dd"
+  TEAMS_WEBHOOK:
+    "https://senlinea.webhook.office.com/webhookb2/34a9dbd2-ebc4-48c4-8821-9497fa2c3b47@274e76db-8d04-4110-abca-44dc583def79/IncomingWebhook/43f7251bcf3c4dfe923500adc52837cc/523e4558-3102-4d0d-b633-e00a21cc58dd",
 };
 
 export { enviroment };
---
+++ src/services/iclient/payments/getObligationPayments/index.ts
@@ -0,0 +1,74 @@
+import { enviroment } from "@config/enviroment";
+import { developmentUsersMock } from "@mocks/users/users.mocks";
+import { mapObligationPaymentsApiToEntities } from "./mappers";
+import { IPayment } from "src/model/entity/payment";
+
+const getObligationPayments = async (
+  userIdentification: string,
+  accessToken: string,
+): Promise<IPayment[]> => {
+  const maxRetries = 5;
+  const fetchTimeout = 3000;
+
+  for (let attempt = 1; attempt <= maxRetries; attempt++) {
+    try {
+      const queryParams = new URLSearchParams({
+        customerPublicCode:
+          developmentUsersMock[userIdentification] || userIdentification,
+      });
+
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => controller.abort(), fetchTimeout);
+
+      const options: RequestInit = {
+        method: "GET",
+        headers: {
+          Realm: enviroment.REALM,
+          Authorization: `Bearer ${accessToken}`,
+          "X-Action": "SearchAllPortfolioObligationPayment",
+          "X-Business-Unit": enviroment.BUSINESS_UNIT,
+          "Content-type": "application/json; charset=UTF-8",
+        },
+        signal: controller.signal,
+      };
+
+      const res = await fetch(
+        `${
+          enviroment.ICLIENT_API_URL_QUERY
+        }/portfolio-obligations?${queryParams.toString()}`,
+        options,
+      );
+
+      clearTimeout(timeoutId);
+
+      if (res.status === 204) {
+        return [];
+      }
+
+      if (!res.ok) {
+        throw {
+          message: "Error al obtener los pagos de obligaciones.",
+          status: res.status,
+        };
+      }
+
+      const data = await res.json();
+
+      const normalizedObligationPayments = Array.isArray(data)
+        ? mapObligationPaymentsApiToEntities(data)
+        : [];
+
+      return normalizedObligationPayments;
+    } catch (error) {
+      if (attempt === maxRetries) {
+        throw new Error(
+          "Todos los intentos fallaron. No se pudo obtener los pagos de obligaciones.",
+        );
+      }
+    }
+  }
+
+  return [];
+};
+
+export { getObligationPayments };
