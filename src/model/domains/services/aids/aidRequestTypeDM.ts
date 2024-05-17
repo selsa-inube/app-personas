e79a658ad1b21b1aa318de9d436d067a602b543f
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2024-05-17 10:37:58 -0500

    feat(aids): dynamic redirect to aid

--- src/model/domains/services/aids/aidRequestTypeDM.ts
+++ src/model/domains/services/aids/aidRequestTypeDM.ts
@@ -2,47 +2,47 @@ import { convertDomainToList, convertDomainToOptions } from "../../helper";
 
 const aidRequestTypeDataDomain = {
   HOSPITALIZATION_SURGERY: {
-    id: "HOSPITALIZATION_SURGERY",
+    id: "hospitalization_surgery",
     value: "Hospitalizacion y cirugia",
   },
   DIAGNOSTIC_AIDS: {
-    id: "DIAGNOSTIC_AIDS",
+    id: "diagnostic_aids",
     value: "Ayudas diagnosticas",
   },
   ORTHOPEDIC_IMPLEMENTS: {
-    id: "ORTHOPEDIC_IMPLEMENTS",
+    id: "orthopedic_implements",
     value: "Por implementos ortopedicos",
   },
   INITIAL_DISABILITY: {
-    id: "INITIAL_DISABILITY",
+    id: "initial_disability",
     value: "Incapacidad inicial",
   },
   DEATH_ASSOCIATE: {
-    id: "DEATH_ASSOCIATE",
+    id: "death_associate",
     value: "Fallecimiento del asociado",
   },
   ADDICTION_PROBLEMS: {
-    id: "ADDICTION_PROBLEMS",
+    id: "addiction_problems",
     value: "Problemas de adiccion",
   },
   PARTIAL_DAMAGE_HOUSE: {
-    id: "PARTIAL_DAMAGE_HOUSE",
+    id: "partial_damage_house",
     value: "Daño parcial de vivienda",
   },
   TOTAL_HOUSING_DAMAGE: {
-    id: "TOTAL_HOUSING_DAMAGE",
+    id: "total_housing_damage",
     value: "Daño total de vivienda",
   },
   DEATH_BENEFICIARY: {
-    id: "DEATH_BENEFICIARY",
+    id: "death_beneficiary",
     value: "Fallecimiento de beneficiario",
   },
   EXHUMATION_OSSUARY_ASSISTANCE: {
-    id: "EXHUMATION_OSSUARY_ASSISTANCE",
+    id: "exhumation_ossuary_assistance",
     value: "Auxilio por exhumacion u osario",
   },
   ORTHOPEDIC_BOOTS: {
-    id: "ORTHOPEDIC_BOOTS",
+    id: "orthopedic_boots",
     value: "Botas ortopedicas",
   },
   MEDICATIONS: {
@@ -50,31 +50,31 @@ const aidRequestTypeDataDomain = {
     value: "Medicamentos",
   },
   MEDICAL_TRANSFERS: {
-    id: "MEDICAL_TRANSFERS",
+    id: "medical_transfers",
     value: "Traslados medicos",
   },
   TEMPORARY_CARE_ACCOMPANIMENT: {
-    id: "TEMPORARY_CARE_ACCOMPANIMENT",
+    id: "temporary_care_accompaniment",
     value: "Acompañamiento asistencial temporal",
   },
   LENSES: {
-    id: "LENSES",
+    id: "lenses",
     value: "Lentes",
   },
   INCAPACITY_180_DAYS: {
-    id: "INCAPACITY_180_DAYS",
+    id: "incapacity_180_days",
     value: "Incapacidad superior a 180 dias",
   },
   AIDS_SPORTS_ARTISTIC_ACTIVITIES: {
-    id: "AIDS_SPORTS_ARTISTIC_ACTIVITIES",
+    id: "aids_sports_artistic_activities",
     value: "Auxilios para actividades deportivas o artisticas",
   },
   DISABILITY_EXTENSION: {
-    id: "DISABILITY_EXTENSION",
+    id: "disability_extension",
     value: "Incapacidad prorroga",
   },
   DAMAGE_BASIC_HOUSEHOLD: {
-    id: "DAMAGE_BASIC_HOUSEHOLD",
+    id: "damage_basic_household",
     value: "Daño de enseres basicos",
   },
 };
