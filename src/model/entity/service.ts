4e49245bac9e9e224bc989e084425610398d53b6
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2024-05-21 09:33:27 -0500

    fix: remove duplicated interface

--- src/model/entity/service.ts
+++ src/model/entity/service.ts
@@ -18,12 +18,4 @@ interface IAid {
   validations: IValidations;
 }
 
-interface IValidation {
-  id: string;
-  label: string;
-  failDetails?: string;
-  value: "fail" | "success" | "pending";
-  isRequired?: boolean;
-}
-
 export type { IAid, IValidation };
