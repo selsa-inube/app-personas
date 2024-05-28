74da301f7cb72c68a9e964d896b933ffa6deedeb
CristianRojas-47 <crisrouges47@gmail.com>
2024-05-28 11:57:58 -0500

    feat(userCard): create userCard component

---
+++ src/components/cards/UserCard/UserCard.stories.tsx
@@ -0,0 +1,47 @@
+import { ThemeProvider } from "styled-components";
+import { UserCard, UserCardProps } from ".";
+
+import { themes } from "@mocks/design/themes";
+import { action } from "@storybook/addon-actions";
+import { StoryFn } from "@storybook/react";
+import { BrowserRouter } from "react-router-dom";
+import { props } from "./props";
+
+const story = {
+  title: "components/cards/UserCard",
+  components: [UserCard],
+  tags: ["autodocs"],
+  argTypes: {
+    ...props,
+  },
+  decorators: [
+    (Story: StoryFn) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+};
+
+export const Default: StoryFn<UserCardProps> = (args) => <UserCard {...args} />;
+Default.args = {
+  name: "David Leonardo Garzón Páramo",
+  documentType: "C.C",
+  identification: "1.013.614.213",
+  onClick: action("Redirect to user"),
+};
+
+const theme = {
+  ...themes["fondecom"],
+};
+
+export const Themed: StoryFn<UserCardProps> = (args) => (
+  <ThemeProvider theme={theme}>
+    <UserCard {...args} />
+  </ThemeProvider>
+);
+Themed.args = {
+  ...Default.args,
+};
+
+export default story;
---
+++ src/components/cards/UserCard/props.ts
@@ -0,0 +1,13 @@
+const props = {
+  name: {
+    description: "Corresponds to the username",
+  },
+  documentType: {
+    description: "Corresponds to the user’s document type",
+  },
+  identification: {
+    description: "Corresponds to the user’s identification number",
+  },
+};
+
+export { props };
