4d391d1582aa70614e544d70e2eb89abaa94a920
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2024-06-04 07:35:32 -0500

    feat(recharges): added recharges routes

--- src/App.tsx
+++ src/App.tsx
@@ -27,12 +27,13 @@ import { CardsProvider } from "./context/cards";
 import { CreditsProvider } from "./context/credits";
 
 import { PageNotFound } from "@components/layout/PageNotFound";
+import { SwitchUser } from "@pages/admin/switchUser";
 import { AppProvider } from "./context/app";
 import { SavingsProvider } from "./context/savings";
 import { AidRoutes } from "./routes/aid";
 import { MyCardsRoutes } from "./routes/myCards";
 import { PaymentsRoutes } from "./routes/payments";
-import { SwitchUser } from "@pages/admin/switchUser";
+import { RechargesRoutes } from "./routes/recharges";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -51,6 +52,8 @@ const router = createBrowserRouter(
         <Route path="my-cards/*" element={<MyCardsRoutes />} />
 
         <Route path="payments/*" element={<PaymentsRoutes />} />
+
+        <Route path="recharges/*" element={<RechargesRoutes />} />
 
         <Route path="credits/*" element={<CreditRoutes />} />
 
---
+++ src/routes/recharges.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+
+function RechargesRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<></>} />
+      <Route path="/history" element={<></>} />
+    </Routes>
+  );
+}
+
+export { RechargesRoutes };
