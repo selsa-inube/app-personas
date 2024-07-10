c95b79ceb10d97ac1ac1b63bb2a4bb9e654800f9
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2024-07-10 13:26:29 -0500

    feat(requests): add my requests page

---
+++ src/pages/admin/requests/MyRequests/EmptyRecords/index.tsx
@@ -0,0 +1,35 @@
+import { OutlineCard } from "@components/cards/OutlineCard";
+import { Icon } from "@design/data/Icon";
+import { Text } from "@design/data/Text";
+import { Stack } from "@design/layout/Stack";
+import { MdSearch } from "react-icons/md";
+
+function EmptyRecords() {
+  return (
+    <OutlineCard>
+      <Stack
+        direction="column"
+        padding="s250"
+        gap="s250"
+        alignItems="center"
+        width="100%"
+        justifyContent="center"
+      >
+        <Icon
+          icon={<MdSearch />}
+          appearance="gray"
+          size="40px"
+          spacing="none"
+        />
+        <Text type="title" size="medium">
+          Sin registros
+        </Text>
+        <Text type="body" size="medium" appearance="gray">
+          Aun no has realizado ninguna solicitud.
+        </Text>
+      </Stack>
+    </OutlineCard>
+  );
+}
+
+export { EmptyRecords };
---
+++ src/pages/admin/requests/MyRequests/config/attributeRecord.ts
@@ -0,0 +1,16 @@
+import { IRequest } from "src/model/entity/request";
+
+const staticAttributes = [
+  { id: "product", label: "Producto", value: "" },
+  { id: "destination", label: "Destino", value: "" },
+  { id: "trackingCode", label: "Código de seguimiento", value: "" },
+  { id: "requestDate", label: "Fecha de solicitud", value: "" },
+];
+
+const generateAttributes = (request: IRequest) =>
+  staticAttributes.map((attr) => ({
+    ...attr,
+    value: request[attr.id as keyof IRequest] as string | number | Date,
+  }));
+
+export { generateAttributes };
