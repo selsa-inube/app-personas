ce147a91180702c9712fae9ab0d1ad5a2473a25f
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2024-08-26 11:36:24 -0500

    refactor(aidRequest): adjust validations layout

--- src/pages/request/aids/AidRequest/forms/RegulationValidationsForm/interface.tsx
+++ src/pages/request/aids/AidRequest/forms/RegulationValidationsForm/interface.tsx
@@ -1,36 +1,85 @@
 import { ValidationCard } from "@components/cards/ValidationCard";
+import { Text } from "@design/data/Text";
+import { inube } from "@design/tokens";
 import { useMediaQuery } from "@hooks/useMediaQuery";
+import { Grid } from "@inubekit/grid";
+import { Stack } from "@inubekit/stack";
 import { FormikProps } from "formik";
 import { IRegulationValidationsEntry } from "./types";
-import { Grid } from "@inubekit/grid";
-import { inube } from "@design/tokens";
 
 interface RegulationValidationsFormUIProps {
   formik: FormikProps<IRegulationValidationsEntry>;
+  loadingValids: boolean;
 }
 
 function RegulationValidationsFormUI(props: RegulationValidationsFormUIProps) {
-  const { formik } = props;
+  const { formik, loadingValids } = props;
 
   const isTablet = useMediaQuery("(max-width: 1100px)");
+  const isMobile = useMediaQuery("(max-width: 580px)");
+
+  const requiredValidations = formik.values.validations.filter(
+    (validation) => validation.isRequired,
+  );
+
+  const notRequiredValidations = formik.values.validations.filter(
+    (validation) => !validation.isRequired,
+  );
 
   return (
-    <Grid
-      templateColumns={`repeat(${isTablet ? 1 : 2}, 1fr)`}
-      autoRows="auto"
-      gap={inube.spacing.s200}
-      width="100%"
-    >
-      {formik.values.validations.map((validation) => (
-        <ValidationCard
-          id={validation.id}
-          label={validation.label}
-          failDetails={validation.failDetails}
-          value={validation.value}
-          key={validation.id}
-        />
-      ))}
-    </Grid>
+    <Stack direction="column" gap={inube.spacing.s400}>
+      {requiredValidations.length > 0 && (
+        <Stack direction="column" gap={inube.spacing.s200}>
+          <Text type="title" size="medium" appearance="gray">
+            Obligatorias en la solicitud
+          </Text>
+
+          <Grid
+            templateColumns={`repeat(${isMobile ? 1 : isTablet ? 2 : 3}, 1fr)`}
+            autoRows="auto"
+            gap={inube.spacing.s200}
+            width="100%"
+          >
+            {requiredValidations.map((validation) => (
+              <ValidationCard
+                id={validation.id}
+                label={validation.label}
+                failDetails={validation.failDetails}
+                value={validation.value}
+                key={validation.id}
+                pending={loadingValids}
+              />
+            ))}
+          </Grid>
+        </Stack>
+      )}
+
+      {notRequiredValidations.length > 0 && (
+        <Stack direction="column" gap={inube.spacing.s200}>
+          <Text type="title" size="medium" appearance="gray">
+            Por validar durante el trámite
+          </Text>
+
+          <Grid
+            templateColumns={`repeat(${isMobile ? 1 : isTablet ? 2 : 3}, 1fr)`}
+            autoRows="auto"
+            gap={inube.spacing.s200}
+            width="100%"
+          >
+            {notRequiredValidations.map((validation) => (
+              <ValidationCard
+                id={validation.id}
+                label={validation.label}
+                failDetails={validation.failDetails}
+                value={validation.value}
+                key={validation.id}
+                pending={loadingValids}
+              />
+            ))}
+          </Grid>
+        </Stack>
+      )}
+    </Stack>
   );
 }
 
