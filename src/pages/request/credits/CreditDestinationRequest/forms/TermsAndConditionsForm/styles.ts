d71b4e55660f57969c0a846da7f370938d813c0b
CristianRojas-47 <crisrouges47@gmail.com>
2024-08-26 11:48:15 -0500

    feat(link): replace link component from inubekit

--- src/pages/request/credits/CreditDestinationRequest/forms/TermsAndConditionsForm/styles.ts
+++ src/pages/request/credits/CreditDestinationRequest/forms/TermsAndConditionsForm/styles.ts
@@ -1,5 +1,4 @@
 import { inube } from "@design/tokens";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 interface IStyledTermsAndConditionsContainer {
@@ -46,18 +45,4 @@ const StyledTermsAndConditionsInfo = styled.div<IStyledTermsAndConditionsInfo>`
     $isMobile ? inube.spacing.s150 : inube.spacing.s200};
 `;
 
-const StyledLinkPolicy = styled(Link)`
-  color: ${({ theme }) =>
-    theme.color?.text?.primary?.regular || inube.color.text.primary.regular};
-
-  &:hover {
-    color: ${({ theme }) =>
-      theme.color?.text?.primary?.hover || inube.color.text.primary.hover};
-  }
-`;
-
-export {
-  StyledLinkPolicy,
-  StyledTermsAndConditionsContainer,
-  StyledTermsAndConditionsInfo,
-};
+export { StyledTermsAndConditionsContainer, StyledTermsAndConditionsInfo };
