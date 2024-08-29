96bba95fa7fec76384fa30a2af567adaf2bc27b1
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2024-08-29 10:37:41 -0500

    feat: add document type

--- src/pages/request/aids/AidRequest/config/mappers.ts
+++ src/pages/request/aids/AidRequest/config/mappers.ts
@@ -35,7 +35,7 @@ const mapRegulationValidations = (
 ): IRegulationValidationsEntry => {
   const selectedAid = aidsRequestMock.find((aid) => aid.id === aidType?.id);
   const validations: IValidation[] = [];
-
+  console.log(selectedAid);
   if (selectedAid) {
     selectedAid.validations.regulations.forEach((regulation) => {
       validations.push({
@@ -43,6 +43,7 @@ const mapRegulationValidations = (
         label: regulation.label,
         value: "pending",
         isRequired: regulation.isRequired,
+        documentType: regulation.documentType,
       });
     });
   }
