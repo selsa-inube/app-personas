81a862606101a683a6463db33177e1800aa9df2f
Sebastian Lara <jsebastianlara3245@gmail.com>
2024-09-16 12:47:09 -0500

    feat(transferModal): avoid touch border

--- src/components/modals/transfers/RechargeModal/styles.ts
+++ src/components/modals/transfers/RechargeModal/styles.ts
@@ -7,7 +7,7 @@ interface IStyledModal {
 
 const StyledModal = styled.div<IStyledModal>`
   display: flex;
-  width: ${({ $smallScreen }) => ($smallScreen ? "312px" : "450px")};
+  width: ${({ $smallScreen }) => ($smallScreen ? "80%" : "450px")};
   padding: ${inube.spacing.s300};
   flex-direction: column;
   gap: ${inube.spacing.s250};
