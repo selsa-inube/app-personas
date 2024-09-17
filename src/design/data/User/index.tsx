2f1759dcf39be45f3ef9f2c6ffcfdbd6f71db2d6
Juan Sebastian Lara <jsebastianlara3245@gmail.com>
2024-09-17 08:20:22 -0500

    fix(fullNav): show cerrar sesion

--- src/design/data/User/index.tsx
+++ src/design/data/User/index.tsx
@@ -1,7 +1,7 @@
-import { Stack } from "@inubekit/stack";
-import { StyledUser } from "./styles";
 import { Avatar } from "@inubekit/avatar";
+import { Stack } from "@inubekit/stack";
 import { Text } from "@inubekit/text";
+import { StyledUser } from "./styles";
 
 interface UserProps {
   username: string;
@@ -14,25 +14,20 @@ function User(props: UserProps) {
   const { username, businessUnit, onlyAvatar, onClick } = props;
 
   return (
-    <StyledUser onClick={onClick}>
-        {!onlyAvatar && (
-          <Stack direction="column" justifyContent="center">
-            <Text type="label" size="medium" textAlign="center">
-              {username}
+    <StyledUser onClick={onClick} $onlyAvatar={onlyAvatar}>
+      {!onlyAvatar && (
+        <Stack direction="column" justifyContent="center">
+          <Text type="label" size="medium" textAlign="center">
+            {username}
+          </Text>
+          {businessUnit && (
+            <Text type="body" size="small" appearance="gray" textAlign="center">
+              {businessUnit}
             </Text>
-            {businessUnit && (
-              <Text
-                type="body"
-                size="small"
-                appearance="gray"
-                textAlign="center"
-              >
-                {businessUnit}
-              </Text>
-            )}
-          </Stack>
-        )}
-        <Avatar />
+          )}
+        </Stack>
+      )}
+      <Avatar />
     </StyledUser>
   );
 }
