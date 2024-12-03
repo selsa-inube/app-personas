6f51d89a58d67dde85a9854671512b9978f0e058
CristianRojas-47 <crisrouges47@gmail.com>
2024-12-03 10:27:43 -0500

    feat(pqrs): adjust principal page from pqrs

--- src/pages/admin/pqrs/MyPQRS/config/attributeRecord.ts
+++ src/pages/admin/pqrs/MyPQRS/config/attributeRecord.ts
@@ -1,6 +1,7 @@
 import { IPQRS } from "src/model/entity/pqrs";
 
 const staticAttributes = [
+  { id: "type", label: "Tipo", value: "" },
   { id: "motive", label: "Motivo", value: "" },
   { id: "code", label: "Código de seguimiento", value: "" },
   { id: "date", label: "Fecha de solicitud", value: "" },
