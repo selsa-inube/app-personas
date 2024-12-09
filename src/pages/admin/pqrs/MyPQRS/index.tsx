c14ca7ad41e7aa80c888908917e644d063c3a4bb
CristianRojas-47 <crisrouges47@gmail.com>
2024-12-09 17:56:48 -0500

    feat(pqrsVisualization): integrate endpoint of visualization pqrs

--- src/pages/admin/pqrs/MyPQRS/index.tsx
+++ src/pages/admin/pqrs/MyPQRS/index.tsx
@@ -1,26 +1,40 @@
-import { useState, useEffect } from "react";
-import { pqrsHistoryMock } from "@mocks/pqrs/pqrsHistory.mocks";
+import { useState, useEffect, useContext } from "react";
 import { MyPQRSUI } from "./interface";
 import { useNavigate } from "react-router-dom";
+import { AppContext } from "src/context/app";
+import { useAuth } from "@inube/auth";
+import { getPqrsHistory } from "src/services/iclient/pqrs/getPqrsHistory";
+import { IPQRS } from "src/model/entity/pqrs";
 
 const RECORDS_INCREMENT = 4;
 
 function MyPQRS() {
+  const { accessToken } = useAuth();
+  const { user } = useContext(AppContext);
+  const navigate = useNavigate();
+
   const [loading, setLoading] = useState(true);
+  const [pqrsRequests, setPqrsRequests] = useState<IPQRS[]>([]);
   const [visibleRecordsCount, setVisibleRecordsCount] =
     useState(RECORDS_INCREMENT);
 
-  const navigate = useNavigate();
-
   useEffect(() => {
-    const simulateDataFetch = () => {
-      const timer = setTimeout(() => {
-        setLoading(false);
-      }, 1000);
-      return () => clearTimeout(timer);
+    const fetchPqrsRequests = async () => {
+      if (accessToken && user?.identification) {
+        setLoading(true);
+        try {
+          const data = await getPqrsHistory(user.identification, accessToken);
+          setPqrsRequests(data);
+        } catch (error) {
+          console.error("Error al obtener el historico de pqrs", error);
+        } finally {
+          setLoading(false);
+        }
+      }
     };
-    simulateDataFetch();
-  }, []);
+
+    fetchPqrsRequests();
+  }, [accessToken, user?.identification]);
 
   const handleNavigateToPqrsDetails = (id: string) => {
     navigate(`/my-pqrs/details/${id}`);
@@ -30,12 +44,12 @@ function MyPQRS() {
     setVisibleRecordsCount((prevCount) => prevCount + RECORDS_INCREMENT);
   };
 
-  const pqrsRequests = pqrsHistoryMock.slice(0, visibleRecordsCount);
-  const totalRecords = pqrsHistoryMock.length;
+  const visibleRequests = pqrsRequests.slice(0, visibleRecordsCount);
+  const totalRecords = pqrsRequests.length;
 
   return (
     <MyPQRSUI
-      pqrsRequests={pqrsRequests}
+      pqrsRequests={visibleRequests}
       loading={loading}
       totalRecords={totalRecords}
       visibleRecordsCount={visibleRecordsCount}
