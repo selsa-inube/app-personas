c20d3e0e3dbdfb827a3cf2f6be077a4ac4646ddf
Sebastian Lara <jsebastianlara3245@gmail.com>
2025-01-29 13:20:29 -0500

    feat: added proteccion theme file

--- src/mocks/design/themes/index.ts
+++ src/mocks/design/themes/index.ts
@@ -3,9 +3,11 @@ import { corbanca } from "@mocks/design/themes/corbanca";
 import { presente } from "@mocks/design/themes/presente";
 import { resolveThemeColors } from "src/utils/themes";
 import fondecom from "./fondecom.json";
+import proteccion from "./proteccion.json";
 
 const themes = {
   fondecom: resolveThemeColors(fondecom),
+  proteccion: resolveThemeColors(proteccion),
   presente,
   cooservunal,
   corbanca,
