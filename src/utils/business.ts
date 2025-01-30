73c81d0d88c14414fd43dd344a514b8299bd0220
Sebastian Lara <jsebastianlara3245@gmail.com>
2025-01-30 13:27:51 -0500

    test: added logs for get subdomain

--- src/utils/business.ts
+++ src/utils/business.ts
@@ -1,8 +1,10 @@
 function getBusinessUnitSubdomain() {
   const hostname = window.location.hostname;
+  console.log(hostname);
   const parts = hostname.split(".");
 
   if (parts.length >= 3) {
+    console.log(parts[0]);
     return parts[0];
   }
 
