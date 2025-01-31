3dfd1fcaae45544ef6fa5c6006c7be7a584669b4
Sebastian Lara <jsebastianlara3245@gmail.com>
2025-01-31 13:46:01 -0500

    feat(consultingUsers): add bunit filter

--- src/services/featureFlags/getConsultingUsers/index.ts
+++ src/services/featureFlags/getConsultingUsers/index.ts
@@ -1,10 +1,14 @@
 import { IConsultingUser } from "src/model/entity/user";
 import { featureFlagsDB } from "src/services/config/supabase/config";
 import { mapConsultingUsersApiToEntities } from "./mappers";
+import { enviroment } from "@config/enviroment";
 
 const getConsultingUsers = async (): Promise<IConsultingUser[]> => {
   try {
-    const { data } = await featureFlagsDB.from("consulting_users").select(`*`);
+    const { data } = await featureFlagsDB
+      .from("consulting_users")
+      .select(`*`)
+      .eq("business_unit", enviroment.BUSINESS_UNIT);
 
     return Array.isArray(data) ? mapConsultingUsersApiToEntities(data) : [];
   } catch (error) {
