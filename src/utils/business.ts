4951e76dff4828d75447b959edbafe5535490dd6
Sebastian Lara <jsebastianlara3245@gmail.com>
2025-02-03 15:37:41 -0500

    test: get env mode in preview

--- src/utils/business.ts
+++ src/utils/business.ts
@@ -3,7 +3,7 @@ const DEFAULT_BUSINESS_UNIT = "fondecom";
 function getBusinessUnitSubdomain() {
   const hostname = window.location.hostname;
   const parts = hostname.split(".");
-
+  console.log(import.meta.env);
   if (parts.length >= 3) {
     if (parts[0].includes("-")) {
       return DEFAULT_BUSINESS_UNIT;
