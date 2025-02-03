6a1f261bf5c29ed0710ae418e49303b8b4a46ed1
Sebastian Lara <jsebastianlara3245@gmail.com>
2025-02-03 15:22:03 -0500

    feat: add fondecom as default bunit

--- src/utils/business.ts
+++ src/utils/business.ts
@@ -1,12 +1,18 @@
+const DEFAULT_BUSINESS_UNIT = "fondecom";
+
 function getBusinessUnitSubdomain() {
   const hostname = window.location.hostname;
   const parts = hostname.split(".");
 
   if (parts.length >= 3) {
+    if (parts[0].includes("-")) {
+      return DEFAULT_BUSINESS_UNIT;
+    }
+
     return parts[0];
   }
 
-  return "fondecom"; 
+  return DEFAULT_BUSINESS_UNIT;
 }
 
 export { getBusinessUnitSubdomain };
