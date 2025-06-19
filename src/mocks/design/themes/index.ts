430d24f94d9aa7e5d4359f80e6cadfc710673f43
Sebastian Lara <jsebastianlara3245@gmail.com>
2025-06-19 14:01:07 -0500

    feat: added prosel tokens

--- src/mocks/design/themes/index.ts
+++ src/mocks/design/themes/index.ts
@@ -4,6 +4,7 @@ import cooptraiss from "./cooptraiss.json";
 import feproteccion from "./feproteccion.json";
 import fondecom from "./fondecom.json";
 import grancoop from "./grancoop.json";
+import prosel from "./prosel.json";
 
 const themes: Record<string, IThemeData> = {
   fondecom: resolveThemeColors(fondecom),
@@ -11,6 +12,7 @@ const themes: Record<string, IThemeData> = {
   cooptraiss: resolveThemeColors(cooptraiss),
   coopebis: resolveThemeColors(coopebis),
   grancoop: resolveThemeColors(grancoop),
+  prosel: resolveThemeColors(prosel),
 };
 
 export { themes };
