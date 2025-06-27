500940cbf03c4179c2840dfecb7a95fb2314580c
Sebastian Lara <jsebastianlara3245@gmail.com>
2025-06-27 17:16:34 -0500

    fix(savings): show correct type in movements

--- src/services/iclient/savings/getSavings/mappers.ts
+++ src/services/iclient/savings/getSavings/mappers.ts
@@ -25,11 +25,9 @@ const mapSavingProductMovementsApiToEntity = (
 ): IMovement => {
   let type: EMovementType | undefined;
 
-  if (Object.prototype.hasOwnProperty.call(movement, "creditMovementPesos")) {
+  if (Object(movement).creditMovementPesos > 0) {
     type = EMovementType.CREDIT;
-  } else if (
-    Object.prototype.hasOwnProperty.call(movement, "debitMovementPesos")
-  ) {
+  } else if (Object(movement).debitMovementPesos > 0) {
     type = EMovementType.DEBIT;
   } else {
     type = undefined;
