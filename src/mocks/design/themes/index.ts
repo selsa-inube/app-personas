de98d546f78df35f5ad8861345efb997eac17816
Sebastian Lara <jsebastianlara3245@gmail.com>
2025-07-07 10:15:32 -0500

    fix: resolve only theme for selected client

--- src/mocks/design/themes/index.ts
+++ src/mocks/design/themes/index.ts
@@ -1,4 +1,4 @@
-import { IThemeData, resolveThemeColors } from "src/utils/themes";
+import { IThemeData } from "src/utils/themes";
 import coopebis from "./coopebis.json";
 import cooptraiss from "./cooptraiss.json";
 import febanc from "./febanc.json";
@@ -8,13 +8,13 @@ import grancoop from "./grancoop.json";
 import prosel from "./prosel.json";
 
 const themes: Record<string, IThemeData> = {
-  fondecom: resolveThemeColors(fondecom),
-  feproteccion: resolveThemeColors(feproteccion),
-  cooptraiss: resolveThemeColors(cooptraiss),
-  coopebis: resolveThemeColors(coopebis),
-  grancoop: resolveThemeColors(grancoop),
-  prosel: resolveThemeColors(prosel),
-  febanc: resolveThemeColors(febanc),
+  fondecom,
+  feproteccion,
+  cooptraiss,
+  coopebis,
+  grancoop,
+  prosel,
+  febanc,
 };
 
 export { themes };
