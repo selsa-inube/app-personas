e76be2ddd572dbe4f1a0e98a5a1f72d751813821
Sebastian Lara <jsebastianlara3245@gmail.com>
2025-08-29 16:39:39 -0500

    fix: pwa install desktop

--- pwa.ts
+++ pwa.ts
@@ -16,7 +16,26 @@ const configPWA: VitePWAOptions = {
   includeAssets: [],
   includeManifestIcons: true,
   disable: false,
-  manifest: false,
+  manifest: {
+    name: "Personas",
+    short_name: "Personas",
+    start_url: ".",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#317EFB",
+    icons: [
+      {
+        src: "https://storage.googleapis.com/assets-clients/inube/prosel/icons/prosel-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "https://storage.googleapis.com/assets-clients/inube/prosel/icons/prosel-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export { configPWA };
