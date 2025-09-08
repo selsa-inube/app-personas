1017b1ce778b2d38c16d6d6b8ed5ec0d311a952a
Sebastian Lara <jsebastianlara3245@gmail.com>
2025-09-08 08:57:55 -0500

    refactor: change init sentry location

--- src/services/errors/handleErrors.ts
+++ src/services/errors/handleErrors.ts
@@ -1,12 +1,5 @@
 import * as Sentry from "@sentry/react";
 
-const inititializeErrorHandling = () => {
-  Sentry.init({
-    dsn: "https://496cad94e9cd9a6ad4c2afb621055ae1@o4509758391582720.ingest.us.sentry.io/4509963881611264",
-    sendDefaultPii: true,
-  });
-};
-
 const captureNewError = (
   error: unknown,
   extras?: Record<string, unknown>,
@@ -31,4 +24,4 @@ const captureNewError = (
   });
 };
 
-export { captureNewError, inititializeErrorHandling };
+export { captureNewError };
