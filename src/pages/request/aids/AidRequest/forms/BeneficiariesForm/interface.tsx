b4812f106eb3f2dd70d572dedb2385719cecca03
Yamil2717 <71434195+Yamil2717@users.noreply.github.com>
2025-09-19 18:52:55 -0500

    refactor: progress on the new design of the aid application

--- src/pages/request/aids/AidRequest/forms/BeneficiariesForm/interface.tsx
+++ src/pages/request/aids/AidRequest/forms/BeneficiariesForm/interface.tsx
@@ -18,13 +18,13 @@ interface BeneficiariesFormUIProps {
 function BeneficiariesFormUI(props: BeneficiariesFormUIProps) {
   const { formik, loading, serviceDomains, onSelectBeneficiary } = props;
 
-  const isTablet = useMediaQuery("(max-width: 1100px)");
+  //const isTablet = useMediaQuery("(max-width: 1100px)");
   const isMobile = useMediaQuery("(max-width: 750px)");
 
   return (
     <Fieldset legend="Posibles beneficiarios">
       <Grid
-        templateColumns={`repeat(${isMobile ? 1 : isTablet ? 2 : 3}, 1fr)`}
+        templateColumns={`repeat(${isMobile ? 1 : 2}, 1fr)`}
         autoRows="auto"
         gap={inube.spacing.s200}
         width="100%"
